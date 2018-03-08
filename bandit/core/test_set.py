53699ba7f2e2b5f601c30c1c6627a0002650655d
Yohan LEVY <yhnlvy@gmail.com>
2018-03-08 12:27:26 +0200

    Removes core/blacklisting.py module

--- bandit/core/test_set.py
+++ bandit/core/test_set.py
@@ -19,7 +19,6 @@ import importlib
 import logging
 
 
-from bandit.core import blacklisting
 from bandit.core import extension_loader
 
 
@@ -34,7 +33,6 @@ class BanditTestSet(object):
         filtering = self._get_filter(config, profile)
         self.plugins = [p for p in extman.plugins
                         if p.plugin._test_id in filtering]
-        self.plugins.extend(self._load_builtins(filtering, profile))
         self._load_tests(config, self.plugins)
 
     @staticmethod
@@ -70,33 +68,6 @@ class BanditTestSet(object):
             filtered.update(all_blacklist_tests)
         return filtered - exc
 
-    def _load_builtins(self, filtering, profile):
-        '''loads up builtin functions, so they can be filtered.'''
-
-        class Wrapper(object):
-            def __init__(self, name, plugin):
-                self.name = name
-                self.plugin = plugin
-
-        extman = extension_loader.MANAGER
-        blacklist = profile.get('blacklist')
-        if not blacklist:  # not overridden by legacy data
-            blacklist = {}
-            for node, tests in extman.blacklist.items():
-                values = [t for t in tests if t['id'] in filtering]
-                if values:
-                    blacklist[node] = values
-
-        if not blacklist:
-            return []
-
-        # this dresses up the blacklist to look like a plugin, but
-        # the '_checks' data comes from the blacklist information.
-        # the '_config' is the filtered blacklist data set.
-        setattr(blacklisting.blacklist, "_test_id", 'B001')
-        setattr(blacklisting.blacklist, "_checks", blacklist.keys())
-        setattr(blacklisting.blacklist, "_config", blacklist)
-        return [Wrapper('blacklist', blacklisting.blacklist)]
 
     def _load_tests(self, config, plugins):
         '''Builds a dict mapping tests to node types.'''
