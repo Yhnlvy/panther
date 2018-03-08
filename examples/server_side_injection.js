f2e49700c0e4ffd42366beebf88edca87b314c94
Izel Furuncuoglu <izel93@gmail.com>
2018-03-08 17:47:42 +0200

    Improve examples and detection of server side injection.

--- examples/server_side_injection.js
+++ examples/server_side_injection.js
@@ -10,3 +10,15 @@ var multiply = new Function('x', 'y', 'z', 'return x * y * z');
 
 var sum = new global.Function('a', 'b', 'return a + b');
 var multiply = new global.Function('x', 'y', 'z', 'return x * y * z');
+
+
+// It should not catch patterns below.
+
+// For eval
+var dog_sum = dog.global.eval('2+2')
+var pig_sum = pig.eval('33+2')
+
+// For Function()
+var elephant = new Elephant.Function()
+var cat = new cat.global.Function()
+
