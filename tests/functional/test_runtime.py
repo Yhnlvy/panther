658d929a0d44d706c40cad2ee1a6cc4197cacb46
Yohan LEVY <yhnlvy@gmail.com>
2018-03-13 13:31:43 +0200

    Fix tests and PEP8 compliance

--- tests/functional/test_runtime.py
+++ tests/functional/test_runtime.py
@@ -50,7 +50,7 @@ class RuntimeTests(testtools.TestCase):
             self.assertIn("Low: 1", output)
             self.assertIn("High: 0", output)
             self.assertIn("Files skipped (0):", output)
-            self.assertIn("Issue: [B106:never_ever_ever_use_eval]", output)
+            self.assertIn("Issue: [P106:never_ever_ever_use_eval]", output)
             self.assertIn("<stdin>:3", output)
 
     def test_nonexistent_config(self):
--- tests/unit/core/test_context.py
+++ tests/unit/core/test_context.py
@@ -14,10 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import ast
-
 import mock
-import six
 import testtools
 
 from panther.core import context
